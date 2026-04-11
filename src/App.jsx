0822695387e732a5b9522bc9a3d20761d6783ef2
julianertle <julian_ertle@web.de>
2026-04-11 16:23:46 +0200

    removed redundant styles.css and moved App.css to styles dir

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styles/App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
